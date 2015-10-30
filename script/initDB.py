c9c72f16ffdcbf5d4874d5c92717b4e37c6375a4
work <work@localhost.localdomain>
2015-10-30 20:43:06 +0800

    add a table to record download task status

--- script/initDB.py
+++ script/initDB.py
@@ -32,6 +32,21 @@ tableSqlDict = {
                `amount` bigint(20) NOT NULL COMMENT 'deal amount of money',
                UNIQUE KEY `idx_code_date` (`stock_code`, `deal_date`)
             )ENGINE=InnoDB DEFAULT CHARSET=utf8;''',
+        "downloadTaskStatus":'''
+            create table `t_download_task_status` (
+               `id` int(16) NOT NULL AUTO_INCREMENT,
+               `first_run_time` date NOT NULL COMMENT 'first run time.',
+               `last_run_time` date NOT NULL COMMENT 'last run time.',
+               `run_times` int(8) NOT NULL COMMENT 'how many time does this task ran.',
+               `options` varchar(1024) NOT NULL COMMENT 'the task cmd options.',
+               `last_success_code` varchar(16) NOT NULL COMMENT 'last success stock code.',
+               `last_success_start` date NOT NULL COMMENT 'start date.',
+               `last_success_end` date NOT NULL COMMENT 'end date.',
+               `update_time` date NOT NULL COMMENT 'last touch this record.',
+               `status` varchar(16) NOT NULL COMMENT 'indicate this task status.',
+               PRIMARY KEY(`id`)
+            )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+        ''',
             }
 def _createTbl(tableName):
     assert tableName in tableSqlDict
@@ -42,13 +57,14 @@ if '__main__' == __name__:
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--create", type=str, 
-            choices=["db_stock", "tbl_dailyStock", "tbl_stockBasic", "all"],
+            choices=["db_stock", "tbl_dailyStock", "tbl_stockBasic","tbl_downloadTaskStatus", "all"],
             help='''
 Create the database or table.
 all : create all the needed database and tables.
 db_stock : create the database.
 tbl_dailyStock : create the table of name "dailyStock", which records the daily stock info.
-tbl_stockBasic : craete the table of name "stockBasic", which records the stock basic info.
+tbl_stockBasic : create the table of name "stockBasic", which records the stock basic info.
+tbl_downloadTaskStatus : create the table of name "downloadTaskStatus", which records the download task's status, to provide the breakpoint.
             ''', required=True)
     parser.add_argument("-f", "--force", action="store_true",
             help="if this option is set, will do init even if the db/table exists, which leads to data deleted.")
@@ -60,4 +76,6 @@ tbl_stockBasic : craete the table of name "stockBasic", which records the stock 
             _createDB()
         if args.create.lower() in ("tbl_dailystock", "all"):
             _createTbl("dailyStock")
+        if args.create.lower() in ("tbl_downloadtaskstatus", "all"):
+            _createTbl("downloadTaskStatus")
 
